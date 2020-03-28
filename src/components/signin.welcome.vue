9b279a2563e53d6a114078411094c03ffa26b215
Bedirhan Yıldırım <bedirhan.yildirim@stu.fsm.edu.tr>
2020-03-28 20:30:40 +0300

    signin component developed

--- src/components/signin.welcome.vue
+++ src/components/signin.welcome.vue
@@ -1,13 +1,210 @@
 <template>
-    $END$
+  <div id="signin">
+    <div id="form-error" v-if="error">{{ this.errorMessage }}</div>
+    <label for="signin-email" class="input-name">Email</label>
+    <input id="signin-email" type="text" v-model="email" name="email" placeholder="ad.soyad@stu.fsm.edu.tr"/>
+    <label for="password" class="input-name">Şifre</label>
+    <input id="password" type="password" v-model="password" name="password" placeholder="********"/>
+    <div class="button">
+      <button @click="signin" :disabled="!(this.isEmailValid && this.isPassValid)">Giriş Yap</button>
+    </div>
+    <div class="divider"></div>
+    <div class="signup">
+      <span>Hala üye değil misin?</span>
+      <a href="javascript:;" @click="signup">Üye Ol</a>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'signin.welcome'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import store from '../store'
+import router from '../router'
+export default {
+  name: 'signin.welcome',
+  store,
+  router,
+  data: function () {
+    return {
+      email: '',
+      isEmailValid: false,
+      password: '',
+      isPassValid: false,
+      error: '',
+      errorMessage: ''
+    }
+  },
+  watch: {
+    email: function () {
+      const signinEmail = document.querySelector('input[name=email]')
+      if (signinEmail.value.length >= 8) {
+        if (!signinEmail.value.includes('@stu.fsm.edu.tr')) {
+          this.isEmailValid = false
+        } else {
+          this.isEmailValid = true
+        }
+      }
+    },
+    password: function () {
+      const signinPassword = document.querySelector('input[name=password]')
+      if (signinPassword.value.length >= 0 && signinPassword.value.length < 8) {
+        this.isPassValid = false
+      } else {
+        this.isPassValid = true
+      }
+    }
+  },
+  mounted: function () {
+    document.querySelector('input[name=email]').focus()
+  },
+  methods: {
+    signin: function () {
+      if (this.isEmailValid && this.isPassValid) {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+          .catch(err => {
+            this.setError(err.code)
+          })
+          .then(res => {
+            if (res) {
+              this.$store.dispatch('setUser', res.user)
+              router.push('home')
+            }
+          })
+      } else {
+        this.setError('Bir hata oluştu!')
+      }
+    },
+    setError: function (errorCode) {
+      this.error = true
+      switch (errorCode) {
+        case 'auth/user-disabled':
+          this.errorMessage = 'Kullanıcıya erişim kesildi!'
+          break
+        case 'auth/invalid-email':
+          this.errorMessage = 'Email adresinizi girdiğinizden emin olun!'
+          break
+        case 'auth/user-not-found':
+          this.errorMessage = 'Böyle bir kullanıcı bulunamadı!'
+          break
+        case 'auth/wrong-password':
+          this.errorMessage = 'Şifre yanlış!'
+          break
+        default:
+          this.errorMessage = 'Bir hata oluştu!'
+      }
+    },
+    signup: function () {
+      this.$emit('goToSignup')
+    }
   }
+}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+#form-error {
+  color: #721c24;
+  font-size: 14px;
+  font-weight: bold;
+  text-shadow: none;
+  padding: 8px 12px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  background-color: #f8d7da;
+  border: 1px solid #f5c6cb;
+}
+.divider {
+  margin: 20px 0;
+  border-bottom: 2px dashed #e5e5e5;
+}
+#signin {
+  padding: 30px;
+  border-radius: 5px;
+  animation: fadeIn 1s;
+  background-color: #fafafa;
+  border: 1px solid #e5e5e5;
+  -webkit-animation: fadeIn 1s;
+}
+#signin {
+  .input-name {
+    font-size: 14px;
+    color: #004680;
+    font-weight: bold;
+    display: block;
+    margin: 0 0 5px 6px;
+  }
+  input {
+    display: block;
+    min-width: 250px;
+    font-size: 14px;
+    padding: 10px 6px;
+    border-radius: 5px;
+    width: calc(100% - 14px);
+    border: 1px solid #e5e5e5;
+    margin-bottom: 20px;
+  }
+  input:last-child {
+    margin-bottom: 0px!important;
+  }
+  input:focus {
+    box-shadow: 0px 0px 3px 0px rgba(0,70,128,.75);
+  }
+  .button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .button {
+    button {
+      width: 100%;
+      color: #ffffff;
+      display: block;
+      font-size: 14px;
+      box-shadow: none;
+      padding: 10px 40px;
+      border-radius: 5px;
+      font-weight: normal;
+      border: 1px solid #e5e5e5;
+      background-color: #004680;
+    }
+    button:hover:enabled {
+      cursor: pointer;
+    }
+    button:hover:disabled {
+      cursor: not-allowed;
+    }
+    button:disabled {
+      background-color: #7b7b7b;
+    }
+  }
+  span {
+    display: block;
+    font-size: 14px;
+  }
+  .signup {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .signup {
+    span {
+      color: #000000;
+      font-size: 14px;
+      margin-right: 10px;
+    }
+    a {
+      color: #004680;
+      font-weight: bold;
+    }
+  }
+}
+@-webkit-keyframes fadeIn {
+  from { opacity: 0; }
+  to { opacity: 1; }
+}
 
+@keyframes fadeIn {
+  from { opacity: 0; }
+  to { opacity: 1; }
+}
 </style>
--- src/pages/welcome.pages.vue
+++ src/pages/welcome.pages.vue
@@ -3,21 +3,8 @@
     <container display="flex" align-items="center" justify-content="center">
       <div class="content">
         <h3 class="title">Hoşgeldin</h3>
-        <signup v-on:goToSignin="openSignup"></signup>
-        <div id="signin">
-          <input type="text" v-model="email" name="email" placeholder="E-mail"/>
-          <div class="error-message"></div>
-          <input type="password" v-model="password" name="password" placeholder="Şifre"/>
-          <div class="error-message"></div>
-          <div class="button">
-            <button @click="signin">Giriş Yap</button>
-          </div>
-          <div class="divider"></div>
-          <div class="signup">
-            <span>Hala üye değil misin?</span>
-            <a href="javascript:;" @click="openSignup">Üye Ol</a>
-          </div>
-        </div>
+        <signup v-if="currentPage=='signup'" v-on:goToSignin="openSignin"></signup>
+        <signin v-if="currentPage=='signin'" v-on:goToSignup="openSignup"></signin>
         <span v-if="loggedIn" style="margin-top: 15px; display: block; text-align: center">{{this.getUser.email}} <a href="javascript:;" @click="logout">Çıkış Yap</a></span>
       </div>
     </container>
@@ -31,17 +18,14 @@ import store from '../store'
 import { mapGetters } from 'vuex'
 import container from '../components/container'
 import signup from '../components/signup.welcome'
+import signin from '../components/signin.welcome'
 export default {
   name: 'WelcomePage',
-  components: { container, signup },
+  components: { container, signup, signin },
   store,
   data: function () {
     return {
-      email: '',
-      password: '',
-      passwordAgain: '',
-      userMail: '',
-      user: ''
+      currentPage: 'signup'
     }
   },
   computed: {
@@ -51,56 +35,13 @@ export default {
     ])
   },
   mounted: function () {
-    const signinEmail = document.querySelector('#signin input[name=email]')
-    const signinPassword = document.querySelector('#signin input[name=password]')
-
-    signinEmail.addEventListener('keyup', function () {
-      if (this.value.length >= 8) {
-        if (!this.value.includes('@stu.fsm.edu.tr')) {
-          this.nextSibling.style.display = 'block'
-          this.nextSibling.innerText = 'Lütfen okul emaili ile giriş yapınız!'
-        } else {
-          this.nextSibling.style.display = 'none'
-        }
-      }
-    })
-    signinPassword.addEventListener('keyup', function () {
-      if (this.value.length >= 4 && this.value.length < 8) {
-        this.nextSibling.style.display = 'block'
-        this.nextSibling.innerText = 'En az 8 karakter içermelidir!'
-      } else {
-        this.nextSibling.style.display = 'none'
-      }
-    })
   },
   methods: {
+    openSignin: function () {
+      this.currentPage = 'signin'
+    },
     openSignup: function () {
-      console.log('başarılı')
-      document.getElementById('signup').style.display = 'block'
-      document.getElementById('signup').style.opacity = '1'
-      document.getElementById('signin').style.display = 'none'
-      document.getElementById('signin').style.opacity = '0'
-    },
-    signin: function () {
-      if (this.email.includes('@stu.fsm.edu.tr')) {
-        if (this.password.length >= 8) {
-          firebase.auth().signInWithEmailAndPassword(this.email, this.password).catch(function (err) {
-            console.log(err.code, err.message)
-          }).then(res => {
-            this.$store.dispatch('setUser', res.user)
-          })
-        } else {
-          console.log('Şifre kısa!')
-        }
-      } else {
-        console.log('Hatalı email adresi!')
-      }
-    },
-    addError: function (el, message) {
-      var p = document.createElement('p')
-      p.classList.add('error-message')
-      p.innerHTML = message.trim()
-      el.parentNode.insertBefore(p, el.nextSibling)
+      this.currentPage = 'signup'
     },
     logout: function () {
       firebase.auth().signOut().catch(function (err) {
@@ -116,6 +57,7 @@ export default {
 <style lang="scss" scoped>
 .welcome-page {
   display: flex;
+  height: 100vh;
   align-items: center;
   justify-content: center;
 }
@@ -132,107 +74,11 @@ export default {
       transition: opacity 1s ease-out;
     }
     .content {
-      .divider {
-        margin: 20px 0;
-        border-bottom: 2px dashed #e5e5e5;
-      }
       .title {
       text-align: center;
       margin-bottom: 30px;
       }
-      #signup, #signin {
-      padding: 30px;
-      border-radius: 5px;
-      animation: fadeIn 1s;
-      background-color: #fafafa;
-      border: 1px solid #e5e5e5;
-      -webkit-animation: fadeIn 1s;
-      }
-      #signup, #signin {
-        input {
-          display: block;
-          min-width: 250px;
-          font-size: 14px;
-          padding: 10px 6px;
-          border-radius: 5px;
-          width: calc(100% - 14px);
-          border: 1px solid #e5e5e5;
-          margin-bottom: 20px;
-        }
-        input:last-child {
-          margin-bottom: 0px!important;
-        }
-        input:focus {
-          box-shadow: 0px 0px 5px 0px rgba(0,70,128,.75);
-        }
-        .button {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-        .button {
-          button {
-            display: block;
-            font-size: 14px;
-            box-shadow: none;
-            padding: 10px 40px;
-            border-radius: 5px;
-            font-weight: normal;
-            border: 1px solid #e5e5e5;
-            background-color: #ffffff;
-            width: 100%;
-          }
-          button:hover {
-            cursor: pointer;
-          }
-          button:disabled {
-            background-color: #fafafa;
-          }
-        }
-        span {
-          display: block;
-          font-size: 14px;
-        }
-        .signin, .signup {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-        .signin, .signup {
-          span {
-            color: #000000;
-            font-size: 14px;
-            margin-right: 10px;
-          }
-          a {
-            color: #004680;
-            font-weight: bold;
-          }
-        }
-      }
-      #signin {
-        opacity: 0;
-        display: none;
-      }
-      .error-message {
-        font-size: 12px;
-        text-align: left;
-        padding: 3px 5px;
-        margin-top: -15px;
-        margin-bottom: 5px;
-        border-radius: 5px;
-        color: rgba(139,0,0,1);
-      }
     }
   }
-}
-@-webkit-keyframes fadeIn {
-  from { opacity: 0; }
-  to { opacity: 1; }
-}
-
-@keyframes fadeIn {
-  from { opacity: 0; }
-  to { opacity: 1; }
 }
 </style>
